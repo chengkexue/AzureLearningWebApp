520fcd8075b186df5e5cf274ced7dd09a02db856
techer_cheng <techer_cheng@epam.com>
2019-04-16 22:37:27 +0800

    Add Webapi

---
+++ AzureLearningWebApp/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AzureLearningWebApp.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public string Get()
+        {
+            return "Welcome To Web API";
+        }
+        public List<string> Get(int Id)
+        {
+            return new List<string> {
+                "Hello1",
+                "Hello2"
+            };
+        }
+    }
+}
