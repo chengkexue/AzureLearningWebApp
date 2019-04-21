43b9843ea60ed8c8135026e7a93e666faedea410
techer_cheng <techer_cheng@epam.com>
2019-04-21 23:09:57 +0800

    Add swagger api

--- AzureLearningWebApp/Controllers/DepartmentController.cs
+++ AzureLearningWebApp/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AzureLearningWebApp.Models;
+using AzureLearningWebApp.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,16 +11,40 @@ namespace AzureLearningWebApp.Controllers
 {
     public class DepartmentController : ApiController
     {
-        public string Get()
+        //public string Get()
+        //{
+        //    return "Welcome To Web API";
+        //}
+        //public List<string> Get(int Id)
+        //{
+        //    return new List<string> {
+        //        "Hello1",
+        //        "Welcome To Web API"
+        //    };
+        //}
+        /// <summary>
+        /// Get All Dept
+        /// </summary>
+        /// <returns>All Dept List</returns>
+        public List<Dept> GetAll()
         {
-            return "Welcome To Web API";
+            return DeptRepository.GetDepts();
         }
-        public List<string> Get(int Id)
+
+        /// <summary>
+        /// Get a specify Dept
+        /// </summary>
+        /// <param name="Id">The Dept id</param>
+        /// <returns>A specify Dept</returns>
+        public Dept GetDeptById(int id)
         {
-            return new List<string> {
-                "Hello1",
-                "Hello2"
-            };
+            var dept = DeptRepository.GetDepts().Where(m => m.Id.Equals(id)).FirstOrDefault();
+            if (dept != null)
+            {
+                dept.Name = "Microsoft";
+                dept.Remark = "Microsoft";
+            }
+            return dept;
         }
     }
 }
---
+++ AzureLearningWebApp/Repository/DeptRepository.cs
@@ -0,0 +1,21 @@
+﻿using AzureLearningWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureLearningWebApp.Repository
+{
+    public class DeptRepository
+    {
+        public static List<Dept> GetDepts()
+        {
+            List<Dept> list = new List<Dept>();
+            list.Add(new Dept() { Id = 1, Name = "WeChat", Remark = "WeChat" });
+            list.Add(new Dept() { Id = 2, Name = "FaceBook", Remark = "FaceBook" });
+            list.Add(new Dept() { Id = 3, Name = "Google", Remark = "Google" });
+            list.Add(new Dept() { Id = 4, Name = "QQ", Remark = "QQ" });
+            return list;
+        }
+    }
+}
